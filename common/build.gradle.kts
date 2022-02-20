440a106534c2d4fc4d9fd9e998aca7a090a2db0c
MasterOfTheFish <fisher1205l@gmail.com>
2022-02-20 12:15:41 -0500

    Bump version number

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.fisher2911"
-version = "1.8.2"
+version = "1.9.1"
 description = "Intuitive, easy-to-use cosmetics plugin, designed for servers using resource packs.\n"
 
 repositories {
