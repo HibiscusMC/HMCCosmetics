bd731c8b6be9da443bb426b4d328dd2667d0a20a
lucian929 <lucian@hibiscusmc.com>
2022-07-08 20:24:24 -0400

    bump version number (1.11.0)!

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.fisher2911"
-version = "1.11.0-BETA-4"
+version = "1.11.0"
 description = "Intuitive, easy-to-use cosmetics plugin, designed for servers using resource packs.\n"
 
 repositories {
