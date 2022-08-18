e3f081f1c96de307c5ec27b183db3960fbc65003
LoJoSho <contact@lojosho.com>
2022-08-18 17:35:48 -0500

    Version Bump (Beta-2)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.fisher2911"
-version = "1.12.0-BETA-1"
+version = "1.12.0-BETA-2"
 description = "Intuitive, easy-to-use cosmetics plugin, designed for servers using resource packs.\n"
 
 repositories {
