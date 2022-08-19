a41fb7df2bef2ca8da7c0393907476b5cb89ab8b
lucian929 <60053521+lucian929@users.noreply.github.com>
2022-08-19 16:23:33 -0400

    Beta-3

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.fisher2911"
-version = "1.12.0-BETA-2"
+version = "1.12.0-BETA-3"
 description = "Intuitive, easy-to-use cosmetics plugin, designed for servers using resource packs.\n"
 
 repositories {
