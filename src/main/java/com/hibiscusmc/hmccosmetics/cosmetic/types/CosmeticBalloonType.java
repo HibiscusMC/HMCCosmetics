f3ba2c4f5a2baf6cb29dc527601fe870003b5638
LoJoSho <contact@lojosho.com>
2022-11-21 21:05:39 -0600

    Removed velocity balloons (it did not work)

--- src/main/java/com/hibiscusmc/hmccosmetics/cosmetic/types/CosmeticBalloonType.java
+++ src/main/java/com/hibiscusmc/hmccosmetics/cosmetic/types/CosmeticBalloonType.java
@@ -8,6 +8,7 @@ import com.hibiscusmc.hmccosmetics.util.packets.PacketManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 import org.spongepowered.configurate.ConfigurationNode;
 
 public class CosmeticBalloonType extends Cosmetic {
@@ -26,12 +27,8 @@ public class CosmeticBalloonType extends Cosmetic {
         Player player = Bukkit.getPlayer(user.getUniqueId());
 
         final Location actual = player.getLocation().clone().add(Settings.getBalloonOffset());
-        //final Location previous = user.getBalloonEntity().getLocation();
-        //final Vector vector = user.getBalloonEntity().getLocation().toVector();
-        //if (vector != null) actual.add(user.getBalloonEntity().getLocation().toVector().multiply(-1));
 
         user.getBalloonEntity().getModelEntity().moveTo(actual.getX(), actual.getY(), actual.getZ());
-        //user.getBalloonEntity().setVelocity(actual.clone().subtract(previous.clone()).toVector());
 
         PacketManager.sendTeleportPacket(user.getBalloonEntity().getPufferfishBalloonId(), actual, false, PlayerUtils.getNearbyPlayers(player));
         PacketManager.sendLeashPacket(user.getBalloonEntity().getPufferfishBalloonId(), player.getEntityId(), PlayerUtils.getNearbyPlayers(player));
