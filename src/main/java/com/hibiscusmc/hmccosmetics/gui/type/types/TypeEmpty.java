58a2411a7c96de5e93e97dd3850d54762d2ecf88
LoJoSho <contact@lojosho.com>
2022-11-25 08:47:51 -0600

    Empty Menu Item Type can now run actions

--- src/main/java/com/hibiscusmc/hmccosmetics/gui/type/types/TypeEmpty.java
+++ src/main/java/com/hibiscusmc/hmccosmetics/gui/type/types/TypeEmpty.java
@@ -1,8 +1,13 @@
 package com.hibiscusmc.hmccosmetics.gui.type.types;
 
+import com.hibiscusmc.hmccosmetics.gui.action.Actions;
 import com.hibiscusmc.hmccosmetics.gui.type.Type;
 import com.hibiscusmc.hmccosmetics.user.CosmeticUser;
 import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.serialize.SerializationException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TypeEmpty extends Type {
 
@@ -12,6 +17,17 @@ public class TypeEmpty extends Type {
 
     @Override
     public void run(CosmeticUser user, ConfigurationNode config) {
-        // Nothing
+        List<String> actionStrings = new ArrayList<>();
+        ConfigurationNode actionConfig = config.node("actions");
+
+        try {
+            if (!actionConfig.node("any").virtual()) actionStrings.addAll(actionConfig.node("any").getList(String.class));
+
+            Actions.runActions(user, actionStrings);
+
+
+        } catch (SerializationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
