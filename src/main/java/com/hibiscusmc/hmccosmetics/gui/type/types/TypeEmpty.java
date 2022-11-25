1e7911b60a76cdbc3258509395dda2ce98260b35
LoJoSho <contact@lojosho.com>
2022-11-25 10:36:22 -0600

    Documentation for item types in menus

--- src/main/java/com/hibiscusmc/hmccosmetics/gui/type/types/TypeEmpty.java
+++ src/main/java/com/hibiscusmc/hmccosmetics/gui/type/types/TypeEmpty.java
@@ -11,23 +11,33 @@ import java.util.List;
 
 public class TypeEmpty extends Type {
 
+    // This can be used as an example for making your own types.
+
     public TypeEmpty() {
         super("empty");
+        // This is an empty type, meaning, when a menu item has a type of "empty" it will run the code in the method run.
     }
 
+    // This is the code that's run when the item is clicked.
     @Override
     public void run(CosmeticUser user, ConfigurationNode config) {
-        List<String> actionStrings = new ArrayList<>();
-        ConfigurationNode actionConfig = config.node("actions");
+        List<String> actionStrings = new ArrayList<>(); // List where we keep the actions the server will execute.
+        ConfigurationNode actionConfig = config.node("actions"); // Configuration node that actions are under.
 
+        // We have to encase it in try catch for configurate serialization
         try {
+            // This gets the actions with the item. We can add more, such as with the Cosmetic type, an equip and unequip action set.
+            // We add that to a List of Strings, before running those actions through the server. This is not the area where we deal
+            // with actions, mearly what should be done for each item.
             if (!actionConfig.node("any").virtual()) actionStrings.addAll(actionConfig.node("any").getList(String.class));
 
+            // We run the actions once we got the raw strings from the config.
             Actions.runActions(user, actionStrings);
-
-
         } catch (SerializationException e) {
             throw new RuntimeException(e);
         }
     }
+
+    // That's it! Now, add it as a static in another one of your classes (such as your main class) and you are good to go.
+    // If you need help with that, check the Types class.
 }
