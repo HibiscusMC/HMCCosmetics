e1f7a2cc0de083459df62d8346959b099ca4b8a1
LoJoSho <contact@lojosho.com>
2022-12-28 10:23:02 -0600

    PAPI work

--- common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/PAPIHook.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/PAPIHook.java
@@ -1,6 +1,9 @@
 package com.hibiscusmc.hmccosmetics.hooks;
 
 import com.hibiscusmc.hmccosmetics.HMCCosmeticsPlugin;
+import com.hibiscusmc.hmccosmetics.cosmetic.Cosmetic;
+import com.hibiscusmc.hmccosmetics.cosmetic.CosmeticSlot;
+import com.hibiscusmc.hmccosmetics.cosmetic.Cosmetics;
 import com.hibiscusmc.hmccosmetics.user.CosmeticUser;
 import com.hibiscusmc.hmccosmetics.user.CosmeticUsers;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
@@ -37,7 +40,24 @@ public class PAPIHook extends PlaceholderExpansion {
         if (parts[0].equalsIgnoreCase("using")) {
             if (parts.length < 2) return null;
             final String id = this.getId(parts, 1);
+            Cosmetic cosmetic = Cosmetics.getCosmetic(id);
+            if (cosmetic == null) return "false";
+            if (user.getCosmetic(cosmetic.getSlot()).equals(cosmetic)) return "true";
         }
+        if (parts[0].equalsIgnoreCase("current")) {
+            if (parts.length < 2) return null;
+            final String id = this.getId(parts, 1);
+            CosmeticSlot cosmeticslot = CosmeticSlot.valueOf(id);
+            if (cosmeticslot == null) return "";
+            if (user.getCosmetic(cosmeticslot) != null) return user.getCosmetic(cosmeticslot).getId();
+        }
+        if (parts[0].equalsIgnoreCase("wardrobe-enabled")) {
+            if (parts.length < 1) return null;
+            if (user.isInWardrobe()) return String.valueOf(user.isInWardrobe());
+            //final String id = this.getId(parts, 1);
+
+        }
+
         return "";
     }
 
