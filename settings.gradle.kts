1c9989e64acbe277c3085ff5ec17f21f8f4676f8
LoJoSho <LoJoShogaming@gmail.com>
2023-02-16 21:23:45 -0600

    readd paper repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
 rootProject.name = "HMCCosmetics"
 include(
     "common",
