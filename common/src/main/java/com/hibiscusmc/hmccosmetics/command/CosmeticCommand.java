ed647b7e5dea433a9c7b139a0a08e07423d66fd8
LoJoSho <LoJoShogaming@gmail.com>
2023-02-21 10:20:42 -0600

    Fix for invalid player on emote command

--- common/src/main/java/com/hibiscusmc/hmccosmetics/command/CosmeticCommand.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/command/CosmeticCommand.java
@@ -389,6 +389,10 @@ public class CosmeticCommand implements CommandExecutor {
                 if (sender.hasPermission("hmccosmetics.cmd.emote.other")) {
                     if (args.length >= 2) player = Bukkit.getPlayer(args[1]);
                 }
+                if (player == null) {
+                    if (!silent) MessagesUtil.sendMessage(sender, "invalid-player");
+                    return true;
+                }
                 CosmeticUser user = CosmeticUsers.getUser(player);
                 if (!user.hasCosmeticInSlot(CosmeticSlot.EMOTE)) {
                     if (!silent) MessagesUtil.sendMessage(sender, "emote-none");
