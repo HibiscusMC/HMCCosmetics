0ddcb12b31c4f39657cac0e0ab10d3ef68a6cf67
Craftinators <craftinatorssx@gmail.com>
2023-03-14 15:47:27 -0400

    feat: abstraction of `PlayerCosmetic...` styled events

---
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/api/CosmeticUserEvent.java
@@ -0,0 +1,26 @@
+package com.hibiscusmc.hmccosmetics.api;
+
+import com.hibiscusmc.hmccosmetics.user.CosmeticUser;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a cosmetic user related event
+ */
+public abstract class CosmeticUserEvent extends Event {
+    protected CosmeticUser user;
+
+    public CosmeticUserEvent(@NotNull final CosmeticUser who) {
+        user = who;
+    }
+
+    /**
+     * Returns the user involved in this event
+     *
+     * @return User who is involved in this event
+     */
+    @NotNull
+    public final CosmeticUser getUser() {
+        return user;
+    }
+}
