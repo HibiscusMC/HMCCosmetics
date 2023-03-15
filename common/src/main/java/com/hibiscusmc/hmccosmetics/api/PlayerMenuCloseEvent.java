af78424b0775c7d80e9595ab6279477b06768154
Craftinators <craftinatorssx@gmail.com>
2023-03-15 15:31:25 -0400

    refactor(PlayerMenuCloseEvent): event no longer cancellable

--- common/src/main/java/com/hibiscusmc/hmccosmetics/api/PlayerMenuCloseEvent.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/api/PlayerMenuCloseEvent.java
@@ -2,38 +2,17 @@ package com.hibiscusmc.hmccosmetics.api;
 
 import com.hibiscusmc.hmccosmetics.gui.Menu;
 import com.hibiscusmc.hmccosmetics.user.CosmeticUser;
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Called when a menu is closed by a player
  */
-public class PlayerMenuCloseEvent extends PlayerMenuEvent implements Cancellable {
+public class PlayerMenuCloseEvent extends PlayerMenuEvent {
     private static final HandlerList handlers = new HandlerList();
-    private boolean cancel = false;
 
     public PlayerMenuCloseEvent(@NotNull CosmeticUser who, @NotNull Menu menu) {
         super(who, menu);
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return cancel;
-    }
-
-    /**
-     * Sets the cancellation state of this event
-     *
-     * <p>
-     * Canceling this event will prevent the player from closing a {@link Menu}
-     * </p>
-     *
-     * @param cancel true if you wish to cancel this event
-     */
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancel = cancel;
     }
 
     @Override
