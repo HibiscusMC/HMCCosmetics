cea40033f98e74ce13938a6d3dbd99926f6ce585
Craftinators <craftinatorssx@gmail.com>
2023-03-15 04:43:36 -0400

    docs(`PlayerWardrobeLeaveEvent`): add class documentation

--- common/src/main/java/com/hibiscusmc/hmccosmetics/api/PlayerWardrobeLeaveEvent.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/api/PlayerWardrobeLeaveEvent.java
@@ -5,6 +5,9 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Called when a player leaves their wardrobe
+ */
 public class PlayerWardrobeLeaveEvent extends CosmeticUserEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
     private boolean cancel = false;
