093b01d7ff2d708c0dfed4ab396082b89adff919
Craftinators <craftinatorssx@gmail.com>
2023-03-16 18:43:52 -0400

    docs(HookLooty): documented class

--- common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/items/HookLooty.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/items/HookLooty.java
@@ -3,18 +3,24 @@ package com.hibiscusmc.hmccosmetics.hooks.items;
 import com.hibiscusmc.hmccosmetics.hooks.Hook;
 import com.mineinabyss.geary.prefabs.PrefabKey;
 import com.mineinabyss.looty.LootyFactory;
-import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
-public class HookLooty extends Hook implements Listener {
-
+/**
+ * A hook that integrates the plugin {@link com.mineinabyss.looty.LootyPlugin Looty} to provide items
+ */
+@SuppressWarnings("SpellCheckingInspection")
+public class HookLooty extends Hook {
     public HookLooty() {
         super("looty");
         setEnabledItemHook(true);
     }
 
+    /**
+     * Gets a cosmetic {@link ItemStack} that is associated with the provided id from the plugin {@link com.mineinabyss.looty.LootyPlugin Looty}
+     */
     @Override
-    public ItemStack getItem(String itemId) {
+    public ItemStack getItem(@NotNull String itemId) {
         PrefabKey prefabKey = PrefabKey.Companion.ofOrNull(itemId);
         if (prefabKey == null) return null;
         return LootyFactory.INSTANCE.createFromPrefab(prefabKey);
