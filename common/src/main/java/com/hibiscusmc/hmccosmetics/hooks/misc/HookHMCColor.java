d0dfa19f2def11b4d383eed9adfd54ea16efa940
Craftinators <craftinatorssx@gmail.com>
2023-03-17 08:53:09 -0400

    docs(HookHMCColor): documented class

--- common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/misc/HookHMCColor.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/misc/HookHMCColor.java
@@ -1,10 +1,11 @@
 package com.hibiscusmc.hmccosmetics.hooks.misc;
 
 import com.hibiscusmc.hmccosmetics.hooks.Hook;
-import org.bukkit.event.Listener;
 
-public class HookHMCColor extends Hook implements Listener {
-
+/**
+ * A hook that integrates the plugin {@link com.hibiscusmc.hmccolor.HMCColor HMCColor}
+ */
+public class HookHMCColor extends Hook {
     public HookHMCColor() {
         super("HMCColor");
     }
