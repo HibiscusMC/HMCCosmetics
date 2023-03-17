3f8f84735fd5532d78ab9a86178c26b779568919
Craftinators <craftinatorssx@gmail.com>
2023-03-17 08:58:39 -0400

    docs(HookPlaceholderAPI): documented class

--- common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/placeholders/HookPlaceholderAPI.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/placeholders/HookPlaceholderAPI.java
@@ -2,14 +2,20 @@ package com.hibiscusmc.hmccosmetics.hooks.placeholders;
 
 import com.hibiscusmc.hmccosmetics.hooks.Hook;
 
+/**
+ * A hook that integrates the plugin {@link me.clip.placeholderapi.PlaceholderAPI PlaceholderAPI}
+ */
 public class HookPlaceholderAPI extends Hook {
-
     public HookPlaceholderAPI() {
         super("PlaceholderAPI");
     }
 
+    /**
+     * @throws RuntimeException If PlaceholderAPI fails to register
+     */
     @Override
-    public void load() {
-        new HMCPlaceholderExpansion().register();
+    public void load() throws RuntimeException {
+        if (!new HMCPlaceholderExpansion().register())
+            throw new RuntimeException("Failed to register PlaceholderExpansion");
     }
 }
