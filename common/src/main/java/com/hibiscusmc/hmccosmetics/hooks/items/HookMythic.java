1d51ee07118abbe175661768c099097418380659
Craftinators <craftinatorssx@gmail.com>
2023-03-17 08:37:52 -0400

    docs(HookMythic): documented class

--- common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/items/HookMythic.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/items/HookMythic.java
@@ -2,17 +2,25 @@ package com.hibiscusmc.hmccosmetics.hooks.items;
 
 import com.hibiscusmc.hmccosmetics.hooks.Hook;
 import io.lumine.mythic.bukkit.MythicBukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
-public class HookMythic extends Hook implements Listener {
+/**
+ * A hook that integrates the plugin {@link io.lumine.mythic.bukkit.MythicBukkit MythicBukkit} to provide cosmetic items
+ */
+@SuppressWarnings("SpellCheckingInspection")
+public class HookMythic extends Hook {
     public HookMythic() {
         super("mythicmobs");
         setEnabledItemHook(true);
     }
 
+
+    /**
+     * Gets a cosmetic {@link ItemStack} that is associated with the provided id from the plugin {@link io.lumine.mythic.bukkit.MythicBukkit MythicBukkit}
+     */
     @Override
-    public ItemStack getItem(String itemId) {
+    public ItemStack getItem(@NotNull String itemId) {
         return MythicBukkit.inst().getItemManager().getItemStack(itemId);
     }
 }
