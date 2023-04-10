3b466f3e025ab68cf0e652dd93662aca6dd1057b
Craftinators <craftinatorssx@gmail.com>
2023-04-10 08:57:31 -0400

    docs(ColorBuilder): documented `#color` method

--- common/src/main/java/com/hibiscusmc/hmccosmetics/util/builder/ColorBuilder.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/util/builder/ColorBuilder.java
@@ -21,6 +21,15 @@ public class ColorBuilder {
                 itemMeta instanceof PotionMeta);
     }
 
+    /**
+     * Applies a {@link Color color} to a {@link PotionMeta} or {@link LeatherArmorMeta} item meta
+     * @param itemMeta The item meta to apply the color to
+     * @param color The color to apply to the item meta
+     * @return The item meta with the color applied, or just the {@code itemMeta} provided if
+     * it wasn't an instanceof {@code PotionMeta} or {@code LeatherArmorMeta}
+     * @see PotionMeta
+     * @see LeatherArmorMeta
+     */
     public static ItemMeta color(ItemMeta itemMeta, final Color color) {
         if (itemMeta instanceof final PotionMeta meta) {
             meta.setColor(color);
