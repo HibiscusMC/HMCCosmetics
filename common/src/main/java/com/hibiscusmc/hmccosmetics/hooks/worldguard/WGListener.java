190f8f776d850f3bfae3c23fa4198721a7b218af
LoJoSho <LoJoShogaming@gmail.com>
2023-06-16 17:37:49 -0500

    feat: WorldGuard listens to PlayerTeleportEvent

--- common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/worldguard/WGListener.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/hooks/worldguard/WGListener.java
@@ -15,6 +15,7 @@ import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -26,6 +27,37 @@ public class WGListener implements Listener {
         CosmeticUser user = CosmeticUsers.getUser(event.getPlayer());
         if (user == null) return;
         Location location = event.getPlayer().getLocation();
+        com.sk89q.worldedit.util.Location loc = BukkitAdapter.adapt(location);
+        RegionContainer region = WorldGuard.getInstance().getPlatform().getRegionContainer();
+        RegionQuery query = region.createQuery();
+        ApplicableRegionSet set = query.getApplicableRegions(loc);
+        if (user.getHidden()) {
+            if (user.getHiddenReason() == CosmeticUser.HiddenReason.WORLDGUARD && set.getRegions().size() == 0) {
+                user.showCosmetics();
+            }
+        }
+        for (ProtectedRegion protectedRegion : set.getRegions()) {
+            if (protectedRegion.getFlags().containsKey(WGHook.getCosmeticEnableFlag())) {
+                if (protectedRegion.getFlags().get(WGHook.getCosmeticEnableFlag()).toString().equalsIgnoreCase("ALLOW")) {
+                    if (user.getHiddenReason() == CosmeticUser.HiddenReason.WORLDGUARD) user.showCosmetics();
+                    return;
+                }
+                user.hideCosmetics(CosmeticUser.HiddenReason.WORLDGUARD);
+                return;
+            }
+            if (protectedRegion.getFlags().containsKey(WGHook.getCosmeticWardrobeFlag())) {
+                if (!WardrobeSettings.getWardrobeNames().contains(protectedRegion.getFlags().get(WGHook.getCosmeticWardrobeFlag()).toString())) return;
+                Wardrobe wardrobe = WardrobeSettings.getWardrobe(protectedRegion.getFlags().get(WGHook.getCosmeticWardrobeFlag()).toString());
+                user.enterWardrobe(true, wardrobe);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+        CosmeticUser user = CosmeticUsers.getUser(event.getPlayer());
+        if (user == null) return;
+        Location location = event.getTo();
         com.sk89q.worldedit.util.Location loc = BukkitAdapter.adapt(location);
         RegionContainer region = WorldGuard.getInstance().getPlatform().getRegionContainer();
         RegionQuery query = region.createQuery();
