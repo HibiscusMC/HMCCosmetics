96817d72794f7a39a825eea0162adafebc16db96
LoJoSho <LoJoShogaming@gmail.com>
2023-07-19 11:09:25 -0500

    clean: update plugin-yml gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("java")
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("xyz.jpenilla.run-paper") version "2.0.0"
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
 }
 
 group = "com.hibiscusmc"
