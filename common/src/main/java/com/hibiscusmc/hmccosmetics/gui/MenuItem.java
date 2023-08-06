a486b13e7ef3538d5360b0b11cfe91b28fe5400f
LoJoSho <LoJoShogaming@gmail.com>
2023-08-05 21:57:31 -0500

    clean: convert MenuItem to record

--- common/src/main/java/com/hibiscusmc/hmccosmetics/gui/MenuItem.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/gui/MenuItem.java
@@ -6,34 +6,6 @@ import org.spongepowered.configurate.ConfigurationNode;
 
 import java.util.List;
 
-public class MenuItem {
-
-    private List<Integer> slots;
-    private ItemStack item;
-    private Type type;
-    private ConfigurationNode itemConfig;
-
-    public MenuItem(List<Integer> slots, ItemStack item, Type type, ConfigurationNode itemConfig) {
-        this.slots = slots;
-        this.item = item;
-        this.type = type;
-        this.itemConfig = itemConfig;
-    }
-
-    public List<Integer> getSlots() {
-        return slots;
-    }
-
-    public ItemStack getItem() {
-        return item;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public ConfigurationNode getItemConfig() {
-        return itemConfig;
-    }
+public record MenuItem(List<Integer> slots, ItemStack item, Type type, ConfigurationNode itemConfig) {
 
 }
