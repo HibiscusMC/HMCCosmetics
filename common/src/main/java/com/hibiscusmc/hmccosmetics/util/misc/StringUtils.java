800d3be25f680cf46d87694e7a7b937d712a6b41
LoJoSho <LoJoShogaming@gmail.com>
2023-11-22 17:04:45 -0600

    clean: removed formatArmorItemType method in StringUtils

--- common/src/main/java/com/hibiscusmc/hmccosmetics/util/misc/StringUtils.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/util/misc/StringUtils.java
@@ -18,19 +18,4 @@ public class StringUtils {
     public static String parseStringToString(final String parsed) {
         return Adventure.SERIALIZER.serialize(Adventure.MINI_MESSAGE.deserialize(parsed));
     }
-
-    @NotNull
-    public static String formatArmorItemType(String type) {
-        type = type.toLowerCase();
-        final String[] parts = type.split(" ");
-
-        final String firstPart = parts[0].substring(0, 1).toUpperCase() + parts[0].substring(1);
-
-        if (parts.length == 1) {
-            return firstPart;
-        }
-
-        return firstPart + parts[1].substring(0, 1).toUpperCase() + parts[1].substring(1);
-    }
-
 }
