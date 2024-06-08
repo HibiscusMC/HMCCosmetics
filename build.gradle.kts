9e8084194a598b93bded0908c88208d6f8e004d0
LoJoSho <LoJoShogaming@gmail.com>
2024-06-08 14:16:02 -0500

    chore: update triumph gui to 3.1.10 (this was not marked as the latest version)

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,7 +94,7 @@ allprojects {
         testCompileOnly("org.projectlombok:lombok:1.18.28")
         testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
 
-        implementation("dev.triumphteam:triumph-gui:3.1.8") {
+        implementation("dev.triumphteam:triumph-gui:3.1.10") {
             exclude("net.kyori") // Already have adventure API
         }
         implementation("com.owen1212055:particlehelper:1.0.0-SNAPSHOT")
