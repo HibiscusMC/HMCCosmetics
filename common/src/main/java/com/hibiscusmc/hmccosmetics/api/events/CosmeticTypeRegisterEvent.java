6818c09a69b44e6fd4d07d310afe395e70bf945c
LoJoSho <LoJoShogaming@gmail.com>
2024-06-14 17:18:59 -0500

    chore: add documentation to CosmeticTypeRegisterEvent

--- common/src/main/java/com/hibiscusmc/hmccosmetics/api/events/CosmeticTypeRegisterEvent.java
+++ common/src/main/java/com/hibiscusmc/hmccosmetics/api/events/CosmeticTypeRegisterEvent.java
@@ -1,21 +1,37 @@
 package com.hibiscusmc.hmccosmetics.api.events;
 
-import lombok.Getter;
 import me.lojosho.shaded.configurate.ConfigurationNode;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Called when a cosmetic type not registered with HMCC default cosmetics is attempted to be registered. So if someone puts "test" in the config slot, and it's not a default cosmetic, this event will be called.
+ */
 public class CosmeticTypeRegisterEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
-    @Getter
     private final String id;
-    @Getter
     private final ConfigurationNode config;
 
     public CosmeticTypeRegisterEvent(String id, ConfigurationNode config) {
         this.id = id;
         this.config = config;
+    }
+
+    /**
+     * Returns the id of the cosmetic trying to be registered. For example, "beanie" or "test"
+     * @return The id. This is the key in the cosmetic config
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * This will already be in the nested node below the id in the config.
+     * @return The cosmetic config node in the cosmetic config that was attempted to get registered
+     */
+    public ConfigurationNode getConfig() {
+        return config;
     }
 
     @Override
