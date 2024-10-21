ad382e5d9e3bdcfef7fa71b103eb8a936e22c8ab
LoJoSho <LoJoShogaming@gmail.com>
2024-10-21 12:28:39 -0500

    chore: update ProtocolLib to newest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ allprojects {
         //compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
         compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
         compileOnly("org.jetbrains:annotations:24.1.0")
-        compileOnly("com.comphenix.protocol:ProtocolLib:5.3.0-SNAPSHOT")
+        compileOnly("com.comphenix.protocol:ProtocolLib:5.3.0")
         compileOnly("me.clip:placeholderapi:2.11.6")
         compileOnly("com.ticxo.modelengine:ModelEngine:R4.0.6")
         compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.12")
