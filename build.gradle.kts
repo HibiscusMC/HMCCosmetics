16e9d09317ae93d9a17cb2ba26227d81a5ea2854
OakLoaf <oak@beaconstudios.org>
2025-06-18 15:58:11 +0100

    Updated PacketEvents to 2.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,7 @@ allprojects {
         //compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
         compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
         compileOnly("org.jetbrains:annotations:24.1.0")
-        compileOnly("com.github.retrooper:packetevents-spigot:2.4.0")
+        compileOnly("com.github.retrooper:packetevents-spigot:2.8.0")
         compileOnly("me.clip:placeholderapi:2.11.6")
         compileOnly("com.ticxo.modelengine:ModelEngine:R4.0.6")
         compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.12")
